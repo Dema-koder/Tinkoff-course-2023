c58c6ed4b87c60c87c8c41996e0b721cdce69d15
Dema-koder <asotruzikovic@gmail.com>
2023-10-05 03:28:04 +0300

    Added Task4 again

--- src/main/java/hw1/Task4.java
+++ src/main/java/hw1/Task4.java
@@ -1,4 +1,29 @@
 package hw1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
+
 public class Task4 {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        LOGGER.info("Read string " + str);
+        LOGGER.info("Result string: " + fixString(str));
+    }
+
+    static String fixString(String str) {
+        char[] charArr = str.toCharArray();
+        for (int i = 0; i < str.length(); i+=2) {
+            if (i != str.length() - 1) {
+                char temp = charArr[i];
+                charArr[i] = charArr[i + 1];
+                charArr[i + 1] = temp;
+            }
+        }
+        String ans = charArr.toString();
+        return ans;
+    }
 }
