87dc31c77e16a06474669a72ae9c118c38788447
Dema-koder <asotruzikovic@gmail.com>
2023-10-15 20:51:36 +0300

    Task 2

--- src/main/java/edu/hw2/Task2.java
+++ src/main/java/edu/hw2/Task2.java
@@ -1,5 +1,38 @@
 package edu.hw2;
 
 public class Task2 {
+    public static class Rectangle {
+        private int width;
+        private int height;
+
+        public Rectangle(int height, int width) {
+            this.height = height;
+            this.width = width;
+        }
+
+        public Rectangle() {
+        }
+
+        public Rectangle setHeight(int height) {
+            return new Rectangle(height, width);
+        }
+
+        public Rectangle setWidth(int width) {
+            return new Rectangle(height, width);
+        }
+
+        double area() {
+            return width * height;
+        }
+    }
+
+    public static class Square extends Rectangle {
+        public Square() {
+        }
+
+        public Square(int side) {
+            super(side, side);
+        }
+    }
 }
 
