73f4327e2bcacb4a9cb39fc0cee2fb1f745c1bbe
Dema-koder <asotruzikovic@gmail.com>
2023-11-11 02:04:41 +0300

    fix checkstyle task 4

--- src/main/java/edu/hw5/Task4.java
+++ src/main/java/edu/hw5/Task4.java
@@ -2,6 +2,7 @@ package edu.hw5;
 
 import java.util.regex.Pattern;
 
+@SuppressWarnings("hideutilityclassconstructor")
 public class Task4 {
     public static boolean isValidPassword(String password) {
         return Pattern.matches(".*[~!@#$%^&*|].*", password);
